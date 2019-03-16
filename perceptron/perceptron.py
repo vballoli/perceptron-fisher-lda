efb39ead7601df185ec440da6919e36920a3ed97
vbbphc <balloli.vb@gmail.com>
2019-03-16 13:33:07 +0530

    Adds Plot code

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,6 +1,9 @@
 from utils.utility import log
 import numpy as np
+import pandas as pd
 import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 class Perceptron:
     """
@@ -17,11 +20,10 @@ class Perceptron:
         self.seed = seed
         self.threshold = threshold
 
-    def predict(self, X, weights=None, info=None):
+    def classify(self, X, weights=None, info=None):
         """
         Single epoch training of the perceptron based on the outputs.
         """
-        log(should_print=self.log, "Predicting: ", info)
         activation = weights[1:] * X + weights[0]
         if activation >= self.threshold:
             prediction = 1
@@ -39,8 +41,8 @@ class Perceptron:
             weights = np.zeros(X.shape[0]+1)
         for epoch in range(self.epochs):
             for input_data, value in zip(X, y):
-                prediction = self.predict(X, y, weights, info="Epoch: " + epoch)
-                improv = (y - prediction) * learning_rate
+                prediction = self.predict(X, weights, info="Epoch: " + epoch)
+                improv = (y - prediction) * self.learning_rate
                 weights[1:] += improv * X
                 weights[0] += improv
                 self.save_model(weights, "weight_"+epoch)
@@ -54,15 +56,23 @@ class Perceptron:
         with open(name, 'wb') as f:
             pickle.dumps(weights, f)
 
-    def predict(self, X_test):
+    def predict(self, X_test, weights, info=None):
         """
         Predicts the labels of the test data based on the trained model.
         """
-        pass
+        return 0
 
     def evaluate(self, y_predicted, y_test):
         """
         Evaluates the performance of the trained perceptron model based on test data.
         """
         pass
+
+
+    def visualize(self, path):
+        df = pd.read_csv(path, names=['x','y'])
+        sns.lmplot('x', 'y', data=df, fit_reg=False)
+        plt.title('Plot')
+        plt.show()
+
 
---
+++ test.py
@@ -0,0 +1,5 @@
+from perceptron.perceptron import Perceptron
+
+p = Perceptron(False, 0.01, 100, 1, 0.5)
+p.visualize('datasets/dataset_1.csv')
+
